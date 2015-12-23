22ddedd4e8a9d3af07a1f62644c78f7e2f2e8453
zhangzhihua <zhangzhihua@zhangzhihuadeimac.local>
2015-12-23 15:54:16 +0800

    Initial Commit

---
+++ TaoBaoShowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaoBaoShowDemo
+//
+//  Created by zhangzhihua on 15/12/23.
+//  Copyright © 2015年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TaoBaoShowDemoTests/TaoBaoShowDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  TaoBaoShowDemoTests.m
+//  TaoBaoShowDemoTests
+//
+//  Created by zhangzhihua on 15/12/23.
+//  Copyright © 2015年 zhangzhihua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TaoBaoShowDemoTests : XCTestCase
+
+@end
+
+@implementation TaoBaoShowDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
